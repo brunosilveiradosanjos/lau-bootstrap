d39c3433af1915c57112824b2051d6f36e00f7a3
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-08-27 01:25:46 -0300

    checkpoint

--- js/plugins.js
+++ js/plugins.js
@@ -5,25 +5,25 @@ $(document).ready(function () { //when document(DOM) loads completely
 });
 
 function checkScroll() { //check if page is scrolled
-if ($(window).scrollTop() >= 300) { //if window is scrolled 300px or more
-    $('.navbar').addClass('solid'); //add class 'solid' to element with class 'navbar'
-} else { //if page is not scrolled 300px from top
-    $('.navbar').removeClass('solid'); //remove class 'solid' from navbar element
-}
+    if ($(window).scrollTop() >= 300) { //if window is scrolled 300px or more
+        $('.navbar').addClass('solid'); //add class 'solid' to element with class 'navbar'
+    } else { //if page is not scrolled 300px from top
+        $('.navbar').removeClass('solid'); //remove class 'solid' from navbar element
+    }
 }
 
 
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
 $(document).ready(function () {
-	checkScroll();
-	$(window).scroll(checkScroll);
-	$('.navbar-toggler').click(function () {
-		if (!$('.navbar').hasClass('solid-toggle')) {
-			if ($(window).scrollTop() <= 300) {
-				$('.navbar').toggleClass('solid-toggle');
-			}
-		}
-	});
+    checkScroll();
+    $(window).scroll(checkScroll);
+    $('.navbar-toggler').click(function () {
+        if (!$('.navbar').hasClass('solid-toggle')) {
+            if ($(window).scrollTop() <= 300) {
+                $('.navbar').toggleClass('solid-toggle');
+            }
+        }
+    });
 });
 
 
@@ -61,7 +61,7 @@ $(document).ready(function () {
 
 
 /*========== MEET THE TEAM CAROUSEL ==========*/
-$(document).ready(function(){ //when document(DOM) loads completely
+$(document).ready(function () { //when document(DOM) loads completely
     $('#team-carousel').owlCarousel({ //owlCarousel settings
         autoplay: true, //set to false to turn off autoplay and only use nav
         autoplayHoverPause: true, //set to false to prevent pausing on hover
@@ -69,19 +69,42 @@ $(document).ready(function(){ //when document(DOM) loads completely
         autoplayTimeout: 8000, //time between transitions
         smartSpeed: 1200, //transition speed
         dotsSpeed: 1000, //transition speed when using dots/buttons
-        responsive : { //set number of items shown per screen width
-            0 : {
+        responsive: { //set number of items shown per screen width
+            0: {
                 items: 1 //0px width and up display 1 item
             },
-            768 : {
+            768: {
                 items: 2 //768px width and up display 2 items
             },
-            992 : {
+            992: {
                 items: 3 //992px width and up display 3 items
             }
         }
     });
-  });
+});
+
+/*========== MEET THE BISOURO CAROUSEL ==========*/
+$(document).ready(function () { //when document(DOM) loads completely
+    $('#bisouro-carousel').owlCarousel({ //owlCarousel settings
+        autoplay: true, //set to false to turn off autoplay and only use nav
+        autoplayHoverPause: true, //set to false to prevent pausing on hover
+        loop: true, //set to false to stop carousel after all slides shown
+        autoplayTimeout: 8000, //time between transitions
+        smartSpeed: 1200, //transition speed
+        dotsSpeed: 1000, //transition speed when using dots/buttons
+        responsive: { //set number of items shown per screen width
+            0: {
+                items: 1 //0px width and up display 1 item
+            },
+            768: {
+                items: 2 //768px width and up display 2 items
+            },
+            992: {
+                items: 3 //992px width and up display 3 items
+            }
+        }
+    });
+});
 
 
 /*========== SKILLS COUNTER ==========*/
@@ -95,7 +118,7 @@ $(document).ready(function () {
 
 
 /*========== CLIENTS CAROUSEL ==========*/
-$(document).ready(function(){ //when document(DOM) loads completely
+$(document).ready(function () { //when document(DOM) loads completely
     $('#clients-carousel').owlCarousel({ //owlCarousel settings
         autoplay: true, //set to false to turn off autoplay and only use nav
         autoplayHoverPause: true, //set to false to prevent pausing on hover
@@ -103,16 +126,16 @@ $(document).ready(function(){ //when document(DOM) loads completely
         autoplayTimeout: 8000, //time between transitions
         smartSpeed: 1600, //transition speed
         dotsSpeed: 1000, //transition speed when using dots/buttons
-        responsive : { //set number of items shown per screen width
-            0 : {
+        responsive: { //set number of items shown per screen width
+            0: {
                 items: 1 //0px width and up display 1 item
             },
-            768 : {
+            768: {
                 items: 2 //768px width and up display 2 items
             }
         }
     });
-  });
+});
 
 /*========== TOP SCROLL BUTTON ==========*/
 $(document).ready(function () {
@@ -138,31 +161,31 @@ $(document).ready(function () {
 /*========== WAYPOINTS ANIMATION DELAY ==========*/
 //Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
 $(function () { // a self calling function
-  function onScrollInit(items, trigger) { // a custom made function
-      items.each(function () { //for every element in items run function
-          var osElement = $(this), //set osElement to the current
-              osAnimationClass = osElement.attr('data-animation'), //get value of attribute data-animation type
-              osAnimationDelay = osElement.attr('data-delay'); //get value of attribute data-delay time
+    function onScrollInit(items, trigger) { // a custom made function
+        items.each(function () { //for every element in items run function
+            var osElement = $(this), //set osElement to the current
+                osAnimationClass = osElement.attr('data-animation'), //get value of attribute data-animation type
+                osAnimationDelay = osElement.attr('data-delay'); //get value of attribute data-delay time
 
-          osElement.css({ //change css of element
-              '-webkit-animation-delay': osAnimationDelay, //for safari browsers
-              '-moz-animation-delay': osAnimationDelay, //for mozilla browsers
-              'animation-delay': osAnimationDelay //normal
-          });
+            osElement.css({ //change css of element
+                '-webkit-animation-delay': osAnimationDelay, //for safari browsers
+                '-moz-animation-delay': osAnimationDelay, //for mozilla browsers
+                'animation-delay': osAnimationDelay //normal
+            });
 
-          var osTrigger = (trigger) ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
+            var osTrigger = (trigger) ? trigger : osElement; //if trigger is present, set it to osTrigger. Else set osElement to osTrigger
 
-          osTrigger.waypoint(function () { //scroll upwards and downwards
-              osElement.addClass('animated').addClass(osAnimationClass); //add animated and the data-animation class to the element.
-          }, {
-                  triggerOnce: true, //only once this animation should happen
-                  offset: '70%' // animation should happen when the element is 70% below from the top of the browser window
-              });
-      });
-  }
+            osTrigger.waypoint(function () { //scroll upwards and downwards
+                osElement.addClass('animated').addClass(osAnimationClass); //add animated and the data-animation class to the element.
+            }, {
+                triggerOnce: true, //only once this animation should happen
+                offset: '70%' // animation should happen when the element is 70% below from the top of the browser window
+            });
+        });
+    }
 
-  onScrollInit($('.os-animation')); //function call with only items
-  onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
+    onScrollInit($('.os-animation')); //function call with only items
+    onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
 });
 
 
@@ -170,45 +193,45 @@ $(function () { // a self calling function
 //Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
 $(function () {
 
-  // init the validator
-  // validator files are included in the download package
-  // otherwise download from http://1000hz.github.io/bootstrap-validator
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
 
-  $('#contact-form').validator();
+    $('#contact-form').validator();
 
 
-  // when the form is submitted
-  $('#contact-form').on('submit', function (e) {
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
 
-      // if the validator does not prevent form submit
-      if (!e.isDefaultPrevented()) {
-          var url = "contact/contact.php";
+        // if the validator does not prevent form submit
+        if (!e.isDefaultPrevented()) {
+            var url = "contact/contact.php";
 
-          // POST values in the background the the script URL
-          $.ajax({
-              type: "POST",
-              url: url,
-              data: $(this).serialize(),
-              success: function (data) {
-                  // data = JSON object that contact.php returns
+            // POST values in the background the the script URL
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data) {
+                    // data = JSON object that contact.php returns
 
-                  // we recieve the type of the message: success x danger and apply it to the
-                  var messageAlert = 'alert-' + data.type;
-                  var messageText = data.message;
+                    // we recieve the type of the message: success x danger and apply it to the
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
 
-                  // let's compose Bootstrap alert box HTML
-                  var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable fade show" role="alert"><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' + messageText + '</div>';
+                    // let's compose Bootstrap alert box HTML
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable fade show" role="alert"><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' + messageText + '</div>';
 
-                  // If we have messageAlert and messageText
-                  if (messageAlert && messageText) {
-                      // inject the alert to .messages div in our form
-                      $('#contact-form').find('.messages').html(alertBox);
-                      // empty the form
-                      $('#contact-form')[0].reset();
-                  }
-              }
-          });
-          return false;
-      }
-  })
+                    // If we have messageAlert and messageText
+                    if (messageAlert && messageText) {
+                        // inject the alert to .messages div in our form
+                        $('#contact-form').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
 });
